773c169683355a48f3b062c1401c52d769f70e2e
judy-chelangat <judysigilai75@gmail.com>
2023-09-20 13:53:32 +0300

    completed assignemnt

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,9 +13,29 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Use db.DateTime and default value
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)  # Use db.DateTime and default value
 
+    baked_goods = db.relationship('BakedGood', back_populates='bakery')
+
+
+    serialize_rules = ('-baked_goods.bakery',)
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price=db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Use db.DateTime and default value
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)  # Use db.DateTime and default value
+    bakery_id =db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+      
+    bakery = db.relationship('Bakery', back_populates='baked_goods')
+
+    serialize_rules = ('-bakery.baked_goods',)
